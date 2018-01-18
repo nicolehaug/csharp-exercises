17eb7e8fa37bfa73e9e5538ce2a2fa0ebfba5be7
Nicole Haug <nichaug@gmail.com>
2018-01-18 07:17:54 -0600

    Added NumericTypes proj -finds area of Rectangle

---
+++ NumericTypes/Program.cs
@@ -0,0 +1,21 @@
+﻿//Program to find out the area of a rectangle with prompt for dimensions from user. By Nicole Haug
+using System;
+
+namespace NumericTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int l, h;
+            Console.Write("To find the area of your rectangle, first enter the length of the Rectangle: ");
+            l = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Now enter the height of the Rectangle: ");
+            h = Convert.ToInt32(Console.ReadLine());
+            int area = l * h;
+           
+            Console.WriteLine("Area of the Rectangle is = {0} ", area);
+            Console.ReadKey();
+        }
+    }
+}
