657ee3bd3d4551b66f73f7be561b476e7801f07f
Nicole Haug <nichaug@gmail.com>
2018-01-18 06:30:39 -0600

    added to HelloWorld to prompt for user name and read it back

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,18 @@
+﻿//Edited HelloWorld program to prompt user for their name and display it out by Nicole Haug
+
+using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! Enter your name:"); //prompt
+            String line = Console.ReadLine(); //Get string from user
+            Console.Write("Hello " + line); //Greets person
+            Console.ReadLine(); //Pauses to read to user
+       
+        }
+    }
+}
